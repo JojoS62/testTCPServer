b60d1f586083665d77bf6a8773eefee956a76998
JojoS62 <sn2010@onlinehome.de>
2020-02-24 21:54:32 +0100

    simplified by removing display stuff version with display is in other branch

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,4 @@
 #include "mbed.h"
-#include <string>
-#include "Adafruit_ST7735.h"
 
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
@@ -8,7 +6,6 @@
 TCPSocket *sockServer;
 SocketAddress mySocketAddress;
 NetworkInterface* network;
-Adafruit_ST7735 display(PB_15, PB_14, PB_10, PB_9, PE_4, PE_5);
 
 void serverStateChanged() 
 {
@@ -84,43 +81,25 @@ void onEthIfDown()
 void onEthIfEvent(nsapi_event_t evt, intptr_t value)
 {
     if (NSAPI_EVENT_CONNECTION_STATUS_CHANGE == evt) {
-        display.setCursor(10, 30);
         switch (value) {
             case NSAPI_STATUS_LOCAL_UP:
-                display.setTextColor(ST7735_GREEN, ST7735_BLACK);
-                display.printf("EthIF local up      ");
                 printf("EthIF local up\n");
                 break;
             case NSAPI_STATUS_GLOBAL_UP:
-                display.setTextColor(ST7735_GREEN, ST7735_BLACK);
-                display.printf("EthIF global up     ");
-                display.setTextColor(ST7735_YELLOW, ST7735_BLACK);
                 printf("EthIF global up\n");
                 network->get_ip_address(&mySocketAddress);
                 printf("my IP is: %s\n", mySocketAddress.get_ip_address());
-                display.setCursor(10, 20);
-                display.printf(mySocketAddress.get_ip_address());
                 onEthIfUp();
                 break;
             case NSAPI_STATUS_DISCONNECTED:
-                display.setTextColor(ST7735_RED, ST7735_BLACK);
-                display.printf("EthIF disconnected  ");
                 printf("EthIF disconnected\n");
-                display.setCursor(10, 20);
-                display.printf("                ");
                 onEthIfDown();
                 break;
             case NSAPI_STATUS_CONNECTING:
                 onEthIfDown();
-                display.setTextColor(ST7735_CYAN, ST7735_BLACK);
-                display.printf("EthIF connecting    ");
                 printf("EthIF connecting\n");
-                display.setCursor(10, 20);
-                display.printf("                ");
                 break;
             case NSAPI_STATUS_ERROR_UNSUPPORTED:
-                display.setTextColor(ST7735_RED, ST7735_BLACK);
-                display.printf("status unknown      ");
                 printf("EthIF status unknown\n");
                 break;
         }
@@ -131,16 +110,6 @@ int main() {
     printf("Hello from "  TOSTRING(TARGET_NAME) "\n");
     printf("Mbed OS version: %d.%d.%d\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
     printf("test TCPServer\n");
-
-    // local display stuff
-    display.initS();
-    display.setRotation(3);
-    display.fillScreen(ST7735_BLACK);
-    display.drawRect(0, 0, 160, 80, ST7735_BLUE);
-    
-    display.setTextColor(ST7735_GREEN);
-    display.setCursor(10, 10);
-    display.printf("Hello");
 
     // Connect to the network with the default networking interface
     // using non blocking, async event driven
